3c8148b642bf4908fc71dbb3bc7591aa7040bf07
Jagadeesh Dachepalli <jagadish.dachepalli@gmail.com>
2023-02-28 12:33:17 +0000

    Final models and other codes

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -1,7 +1,14 @@
-{{ config(materialized="view") }}
-
-select 
--- identifiers
+{{ config(materialized='view') }}
+ 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+  from {{ source('staging','yellow_tripdata') }}
+  where vendorid is not null 
+)
+select
+   -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
@@ -31,11 +38,13 @@ select
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
- from {{ source('staging', 'yellow_tripdata') }}
-where vendorid is not null
+from tripdata
+where rn = 1
+
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
 
   limit 100
 
 {% endif %}
+
